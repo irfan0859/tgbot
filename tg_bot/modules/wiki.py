4edc4d842f3f042a09848f5dee01bcd8061ff6f4
irfan0859 <30881484+irfan0859@users.noreply.github.com>
2018-09-14 19:56:18 +0300

    Update wiki.py

--- tg_bot/modules/wiki.py
+++ tg_bot/modules/wiki.py
@@ -4,8 +4,8 @@ from telegram.ext import run_async, Filters
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 def wiki(bot: Bot, update: Update):
-        query = update.effective_message.text.split(None, 1)
-        result = '**Search:**\n`' + query + '`\n\n**Result:**\n`' + wikipedia.summary(match)
+        query = str(update.effective_message.text[6:])
+        result = '**Search:**\n' + query + '\n\n**Result:**\n' + str(wikipedia.summary(query))
         update.effective_message.reply_markdown(result)
 __help__ = """
  - /wiki: Query the Wikipedia
